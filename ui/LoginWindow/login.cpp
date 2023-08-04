5fe0d0361cdfaf22daea4e71f34007cc11ce1b3c
winlogon <roman.sarkisyan15@gmail.com>
2023-08-04 18:47:56 +0300

    Merge branch 'message+editText'

--- ui/LoginWindow/login.cpp
+++ ui/LoginWindow/login.cpp
@@ -35,49 +35,7 @@ void  Login::on_pushButton_clicked()
 //Login
 void Login::on_button_continue_reg_clicked()
 {
-    if(isFieldsValid)
-    {
-        QSqlDatabase db = QSqlDatabase::addDatabase("QMYSQL");
-        db.setHostName("127.0.0.1");
-        db.setDatabaseName("Space");
-        db.setUserName("root");
-        db.setPassword("winlogon991991_");
-
-        if (!db.open())
-        {
-            QMessageBox::critical(NULL, QObject::tr("Ошибка"), db.lastError().text());
-            return;
-        }
-
-        // Получение введенных пользователем данных
-        QString username = ui->text_username_reg->text();
-        QString password = ui->text_password_reg->text();
-
-        // Создание SQL-запроса INSERT
-        QSqlQuery query;
-        query.prepare("INSERT INTO users (username, email, password) VALUES (:username, :email, :password)");
-        query.bindValue(":username", username);
-        query.bindValue(":password", password);
-
-        if (!query.exec())
-        {
-            QMessageBox::critical(NULL, QObject::tr("Ошибка"), query.lastError().text());
-        }
-        else
-        {
-            application_window->show();
-            db.close();
-            this->close();
-        }
-
-    }
-    else
-    {
-        // Вывод сообщения об ошибке или предупреждения пользователю
-        ui->lineEdit_empty->setText("Заполните все поля");
-        ui->lineEdit_empty->setStyleSheet("color: red;");
-        ui->lineEdit_empty->setVisible(true);
-    }
+    
 }
 
 
