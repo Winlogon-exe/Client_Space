50bf33595bd4ca3cdf69d862ae9653af3f0559ef
winlogon <roman.sarkisyan15@gmail.com>
2023-08-09 15:18:18 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,13 +1,14 @@
-#include "ui/RegWindow/mainwindow.h"
-#include "mainwindow.h"
-
+#include"application.h"
 #include <QApplication>
 #include<thread>
+
+#include "ui/RegWindow/mainwindow.h"
+#include "mainwindow.h"
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    MainWindow app;
+    Application app;
     app.show();
 
 
